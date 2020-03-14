5dc75578c0cfe89c40768969cf15bf5aab4c0554
daniel lukasik <daniel.lukasik10@gmail.com>
2020-03-14 18:16:18 +0100

    added routing and filtering for countries and trips

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
-// TODO - import other views
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
 import Info from './components/views/Info/Info';
+import Trip from './components/views/Trip/TripContainer';
+import Country from './components/views/Country/CountryContainer';
 import NotFound from './components/views/NotFound/NotFound';
+// DONE - import other views
 
 import parseTrips from './utils/parseTrips';
-import {setMultipleStates} from './redux/globalRedux';
+import { setMultipleStates } from './redux/globalRedux';
 
 class App extends React.Component {
   static propTypes = {
@@ -20,28 +24,32 @@ class App extends React.Component {
     setStates: PropTypes.func,
   }
 
-  constructor(props){
+  constructor(props) {
     super(props);
     // parse trips when App is first created
     parseTrips(this.props.trips, this.props.setStates);
   }
 
-  componentDidUpdate(prevProps){
-    if(prevProps.trips != this.props.trips){
+  componentDidUpdate(prevProps) {
+    if (prevProps.trips != this.props.trips) {
       // parse trips again if they changed
       parseTrips(this.props.trips, this.props.setStates);
     }
   }
 
-  render(){
+  render() {
     return (
       <BrowserRouter>
         <MainLayout>
           <Switch location={location}>
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
-            {/* TODO - add more routes for other views */}
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/regions' component={Regions} />
             <Route exact path='/info' component={Info} />
+            <Route exact path='/trip/:id' component={Trip} />
+            <Route exact path='/country/:id' component={Country} />
+            {/* DONE - add more routes for other views */}
             <Route path='*' component={NotFound} />
           </Switch>
         </MainLayout>
